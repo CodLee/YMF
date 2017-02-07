f14032f09d614ceda8621a984fac96d4009b2360
Scott <578591530@qq.com>
2017-02-07 09:09:10 +0800

    上穿了御美坊的主页面及御美品牌,御美企业页面

---
+++ js/myApp.js
@@ -0,0 +1,28 @@
+document.addEventListener("touchmove",function(e){
+    // e.preventDefault();//默认可以下滑
+});
+// pure JS
+var elem = document.getElementById('mySwipe');
+window.mySwipe = Swipe(elem, {
+  startSlide:0,/*开始是第几页*/ 
+  auto: 2000,//自动播放的时间间隔 
+  continuous: true,// 
+  // disableScroll: true,
+  // stopPropagation: true,
+  callback: function(index, element) {
+    $("#mySwipe li").css("background-color","red");
+    $("#mySwipe li").eq(index).css("background-color","#fff");
+  }
+});
+$('.top_btn').on('click',function(){
+  $('.sidebar').toggle('on');
+});
+$('.sidebar').on('swipeLeft',function(e){
+  $(this).removeClass('on');
+  // e.preventDefault();
+  console.info('1234');
+
+});
+$('.sidebar .item').on('click',function(){
+  $(this).addClass('on').siblings().removeClass('on');
+})
